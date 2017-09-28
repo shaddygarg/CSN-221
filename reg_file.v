23ff3d237642ce69c3def388f3af5fed9c790fd8
shu121197 <rmaheshwari05@gmail.com>
2017-09-29 01:36:39 +0530

    Update reg_file.v

--- reg_file.v
+++ reg_file.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/27/2017 12:02:22 PM
-// Design Name: 
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module reg_file(
     input clk,
@@ -31,20 +12,20 @@ module reg_file(
 );
 reg [15:0] reg_number [7:0];
 initial begin
-reg_number[0] <= 15'b0;
-reg_number[1] <= 15'b0;
-reg_number[2] <= 15'b0;
-reg_number[3] <= 15'b0;
-reg_number[4] <= 15'b0;
-reg_number[5] <= 15'b0;
-reg_number[6] <= 15'b0;
-reg_number[7] <= 15'b0;
+    reg_number[0] <= 16'b0;
+    reg_number[1] <= 16'b0;
+    reg_number[2] <= 16'b0;
+    reg_number[3] <= 16'b0;
+    reg_number[4] <= 16'b0;
+    reg_number[5] <= 16'b0;
+    reg_number[6] <= 16'b0;
+    reg_number[7] <= 16'b0;
 end
 always @(*) begin
 if(enable) begin
 reg_number[wr_reg] <= wr_data;
 end
 end
-assign read_data1 = (read_addr1 == 0) ? 15'b0 : reg_number[read_addr1];
-assign read_data2 = (read_addr2 == 0) ? 15'b0 : reg_number[read_addr2];  
+assign read_data1 = (read_addr1 == 0) ? 16'b0 : reg_number[read_addr1];
+assign read_data2 = (read_addr2 == 0) ? 16'b0 : reg_number[read_addr2];  
 endmodule
