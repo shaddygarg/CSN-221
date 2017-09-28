d68a01f142bcfe322e228e977b9c81d4b9bed6d0
shu121197 <rmaheshwari05@gmail.com>
2017-09-29 01:38:34 +0530

    Update alu_control.v

--- alu_control.v
+++ alu_control.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/27/2017 02:31:19 PM
-// Design Name: 
-// Module Name: alu_control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_control(ALU_CONTROL,ALU_OP,ALU_OP_CODE);
 	output reg[3:0] ALU_CONTROL;
